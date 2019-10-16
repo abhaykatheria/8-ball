b17964ee0d64b6b5da7723468ae4be45b6063f24
abhay <abhay.katheria1998@gmail.com>
2019-10-16 20:06:52 +0530

    yay

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: ball(),
       ),
     );
+
+class ball extends StatefulWidget {
+  @override
+  _ballState createState() => _ballState();
+}
+
+class _ballState extends State<ball> {
+  int i = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(
+          child: Text(
+            "8 Ball",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+              letterSpacing: 5.0,
+            ),
+          ),
+        ),
+        titleSpacing: 10.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    i = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset("images/ball$i.png")),
+          ),
+        ],
+      ),
+    );
+  }
+}
